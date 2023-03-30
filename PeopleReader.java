9a76af065359a893aa706a960f39357bd2fc1436
Yanan <Evazhao919@gmail.com>
2023-03-30 12:55:16 -0500

    chengess

--- PeopleReader.java
+++ PeopleReader.java
@@ -16,7 +16,34 @@ public class PeopleReader {
 
         List<String> lines = Files.readAllLines(Path.of(filename));
         for (String line : lines) {
-            people.add(new Person(line));
+            String[] parts= line.split(" ");
+            String fName = parts[0];
+            // String lName = parts[1] + parts[2] + parts [3];
+           String lName = parts[1];
+        int age;
+        String mName;
+        String fear;
+        int counter;
+        // if parts [2]is a number, save it as an int
+        // if parts [2] is a String, parts [1] is middle, parts[2] is last, parts[3] is int
+       try{
+         age = Integer.parseInt(parts[2]);
+         mName = "";
+         counter = 3;
+       }catch(NumberFormatException nfe){
+        mName = parts[1];
+        lName = parts[2];
+        age = Integer.parseInt(parts[3]);
+        counter = 4;
+       }
+
+       if (parts.length - counter == 1){
+        fear = parts[counter];
+       }else{
+         fear = parts[counter] + " " + parts[counter+1];
+       }
+
+            people.add(new Person(fName,mName,lName,age,fear));
         }
 
         return people;
