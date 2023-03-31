8d2d0ea41c2b114f75eae0f687394a72900bdbac
Yanan <Evazhao919@gmail.com>
2023-03-30 23:20:00 -0500

    final

--- App.java
+++ App.java
@@ -1,13 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws IOException {
         PeopleReader reader = new PeopleReader("people.txt");
         List<Person> people = reader.getAllPeople();
+        Map<String,List<Person>> groups = new HashMap<>();
 
+    
         for (Person p : people) {
+            String fear = p.getfear();
+            if (groups.containsKey(fear)){
+                groups.get(fear).add(p);
+            }else{
+                groups.put(fear,new ArrayList<>());
+                groups.get(fear).add(p);
+            }
+        }
+        for(Map.Entry<String,List<Person>> entry : groups.entrySet()){
+            System.out.println(entry.getKey());
+          for(Person p : entry.getValue()){
             System.out.println(p);
+          }
         }
     }
 }
